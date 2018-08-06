ad854da6ab823211702da908c409f925637db7e4
ADAllen <1007715170@qq.com>
2018-08-07 00:28:34 +0800

    更改

--- server.js
+++ server.js
@@ -20,7 +20,15 @@ var server = http.createServer(function(request, response){
   /******** 从这里开始看，上面不要看 ************/
 
   console.log('HTTP 路径为\n' + path)
-  if(path == '/'){
+  if(path == '/style.css'){
+    response.setHeader('Content-Type', 'text/css; charset=utf-8')
+    response.write('body{background-color: #ddd;}h1{color: red;}')
+    response.end()
+  }else if(path == '/main.js'){
+    response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
+    response.write('alert("这是JS执行的")')
+    response.end()
+  }else if(path == '/'){
     response.setHeader('Content-Type', 'text/html; charset=utf-8')
     response.write('<!DOCTYPE>\n<html>'  + 
       '<head><link rel="stylesheet" href="/main.js">' +
